29ae0aea7e082a8cee0ad57c3d60b8e6ad4c47a5
Bryan Vaz <bryanvaz@users.noreply.github.com>
2023-05-29 15:39:06 +0000

    bump to 0.1.2-develop

--- src/__main__.py
+++ src/__main__.py
@@ -39,7 +39,7 @@ import list_vfs
 import text_help
 import sys
 
-version_number = "0.1.1-develop"
+version_number = "0.1.2-develop"
 
 def print_help():
     """Prints the help information for vfnet"""
